59da5d3a56363bc92077a351a8bec3bc9cf62dd0
marianschmidt <marian.schmidt@gmail.com>
2019-10-29 17:35:58 +0100

    v0.5.5 - add new option that converts time_id_var to numeric

--- R/reshape_long_tidyr.R
+++ R/reshape_long_tidyr.R
@@ -6,6 +6,8 @@
 #'                E.g. \code{idvar="PUBCSNUM"} for SEER data.
 #' @param time_id_var String with name of variable that indicates diagnosis per patient.
 #'                E.g. \code{timevar="SEQ_NUM"} for SEER data.
+#' @param time_id_num Logical that indicates whether time_id_var should be transformed to numeric or not.
+#'                Default is \code{time_id_num=TRUE}.
 #' @param constant_vars Vector of variables that are constant for all times per case_id, so these variables will just be filled during pivot_longer
 #'                      Default is NULL.
 #' @param drop_na Logical to indicate whether empty rows after pivoting should be dropped (activates option values_drop_na in tidyr::pivot_longer).
@@ -16,7 +18,7 @@
 #' @export
 #' 
 
-reshape_long_tidyr <- function(df, case_id_var, time_id_var, constant_vars = NULL, drop_na = FALSE, var_selection = c("_all"), names_pattern = "(.*)\\.(.*)"){
+reshape_long_tidyr <- function(df, case_id_var, time_id_var, time_id_num = TRUE, constant_vars = NULL, drop_na = FALSE, var_selection = c("_all"), names_pattern = "(.*)\\.(.*)"){
   
   df %>%
     tidyr::pivot_longer(
@@ -26,7 +28,9 @@ reshape_long_tidyr <- function(df, case_id_var, time_id_var, constant_vars = NUL
       values_drop_na = drop_na
     ) %>%
     #enable variable selection
-    {if (all(var_selection != "_all")){dplyr::select(case_id_var, time_id_var, var_selection)} else {.}}
+    {if (all(var_selection != "_all")){dplyr::select(case_id_var, time_id_var, var_selection)} else {.}} %>% 
+    #make time_id_var numeric if 
+    {if (time_id_num == TRUE){dplyr::mutate_at(time_id_var, as.numeric)} else {.}}
   
   }
 
