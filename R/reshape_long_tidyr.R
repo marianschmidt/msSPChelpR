73d67f89c0e713ad0254e08367fb11bc04e8b694
marianschmidt <marian.schmidt@gmail.com>
2019-10-29 16:38:51 +0100

    v0.5.4 add constant_vars and names_pattern option to reshape_long_tidyr

--- R/reshape_long_tidyr.R
+++ R/reshape_long_tidyr.R
@@ -6,20 +6,23 @@
 #'                E.g. \code{idvar="PUBCSNUM"} for SEER data.
 #' @param time_id_var String with name of variable that indicates diagnosis per patient.
 #'                E.g. \code{timevar="SEQ_NUM"} for SEER data.
-#' @param var_selection Vector of variables to keep. Default is "_all".
+#' @param constant_vars Vector of variables that are constant for all times per case_id, so these variables will just be filled during pivot_longer
+#'                      Default is NULL.
 #' @param drop_na Logical to indicate whether empty rows after pivoting should be dropped (activates option values_drop_na in tidyr::pivot_longer).
 #'                Default is drop_na = FALSE.
+#' @param var_selection Vector of variables to keep after pivoting. Default is "_all".
+#' @param names_pattern A regular expression containing matching groups (()). Default is names_pattern = "(.*)\\.(.*)" where variable name and time_id are separated by a dot ".".
 #' @return df
 #' @export
 #' 
 
-reshape_long_tidyr <- function(df, case_id_var, time_id_var, drop_na = FALSE, var_selection = c("_all")){
+reshape_long_tidyr <- function(df, case_id_var, time_id_var, constant_vars = NULL, drop_na = FALSE, var_selection = c("_all"), names_pattern = "(.*)\\.(.*)"){
   
   df %>%
     tidyr::pivot_longer(
-      -c({{case_id_var}}),
+      -c({{case_id_var}}, {{constant_vars}}),
       names_to = c(".value", {{time_id_var}}),
-      names_pattern = "(.*)\\.(.*)",
+      names_pattern = names_pattern,
       values_drop_na = drop_na
     ) %>%
     #enable variable selection
