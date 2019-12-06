45c68d307567eecfd3533baded070bbba6cc6661
marianschmidt <marian.schmidt@gmail.com>
2019-12-06 17:41:09 +0100

    v0.6.2 improvement to summarize_sir_results by options output_information and shorten_total_cols; multi dimensional wide tables with xbreak_var and fu_time are possible

--- R/summarize_sir_results.R
+++ R/summarize_sir_results.R
@@ -10,9 +10,13 @@
 #'               Or "wide" for wide format where fubreak_var and xbreak_var are appended as columns.
 #'               Or "long" for long format where sir_df is not reshaped, but just summarized (ybreak_var, xbreak_var and fubreak_var remain in rows).
 #'               Default is "long".
+#' @param output_information option to define information to be presented in final output table. Default is "full" information, i.e. all variables from
+#'                           from sir_df. "reduced" is observed, expected, sir, sir_ci / sir_lci+sir_uci, pyar, n_base. "minimal" is observed, expected, sir, sir_ci. 
+#'                           Default is "full".
 #' @param add_total_row option to add a row of totals. Can bei either "no" for not adding such a row or "top" or "bottom" for adding it at the first or last row. Default is "no".
 #' @param add_total_fu option to add totals for follow-up time. Can bei either "no" for not adding such a column or "left" or "right" for adding it at the first or last column. Default is "no".
 #' @param collapse_ci If TRUE upper and lower confidence interval will be collapsed into one column separated by "-". Default is FALSE.
+#' @param shorten_total_cols Shorten text in all results columns that start with "Total". Default == FALSE.
 #' @param fubreak_var_name Name of variable with futime stratification. Default is "fu_time".
 #' @param ybreak_var_name Name of variable with futime stratification. Default is "yvar_name".
 #' @param xbreak_var_name Name of variable with futime stratification. Default is "xvar_name".
@@ -23,12 +27,14 @@ summarize_sir_results <- function(sir_df,
                                   summarize_groups,
                                   summarize_icdcat,
                                   output = "long",
+                                  output_information = "full",
                                   add_total_row = "no",
                                   add_total_fu = "no",
                                   collapse_ci = FALSE,
+                                  shorten_total_cols = FALSE,
                                   fubreak_var_name = "fu_time",
                                   ybreak_var_name = "yvar_name",
-                                  xbreak_var_name = "xvar_name",
+                                  xbreak_var_name = "none",
                                   alpha = 0.05
 ) {
   
@@ -96,6 +102,22 @@ summarize_sir_results <- function(sir_df,
     }
   }
   
+  #prepare sorting
+  if(yb & !("yvar_sort" %in% colnames(sir_df))){
+    sir_df <- sir_df %>%
+      dplyr::mutate(yvar_sort = as.numeric(as.factor(.data[[!!ybreak_var_name]]))) 
+  }
+  
+  if(yb & !("yvar_sort_levels" %in% colnames(sir_df))){
+    sir_df <- sir_df %>%
+      dplyr::mutate(yvar_sort_levels = as.numeric(as.factor(.data[[!!ylabel_var_name]]))) 
+  }
+  
+  if(fu & !("fu_time_sort" %in% colnames(sir_df))){
+    sir_df <- sir_df %>%
+      dplyr::mutate(fu_time_sort = as.numeric(as.factor(.data[[!!fubreak_var_name]]))) 
+  }
+  
   
   #prepare summarize_groups
   
@@ -115,18 +137,8 @@ summarize_sir_results <- function(sir_df,
   
   if(add_total_row == "top" | add_total_row == "bottom"){
     
-    df <- df %>%
-      dplyr::mutate(total_var = "Overall")
+    #BUG: do something here
     
-    length_yb = length_yb + 1
-    
-    if(add_total_row == "top") {
-      ybreak_var_names <- c("total_var", ybreak_var_names)
-    }
-    
-    if(add_total_row == "bottom") {
-      ybreak_var_names <- c(ybreak_var_names, "total_var")
-    }
   }
   
   #prepare collapse_ci option
@@ -208,8 +220,8 @@ summarize_sir_results <- function(sir_df,
     
     #ii) create vector with all possible grouping vars and CHK
     all_grouping_vars <- c("age", "sex", "region", "year", 
-                           if(fu){fubreak_var_name},
-                           if(yb){c(ybreak_var_name, ylabel_var_name)}, if(xb){c(xbreak_var_name, xlabel_var_name)})
+                           if(fu){c(fubreak_var_name, "fu_time_sort")},
+                           if(yb){c(ybreak_var_name, ylabel_var_name, "yvar_sort", "yvar_sort_levels")}, if(xb){c(xbreak_var_name, xlabel_var_name)})
     
     sg_not_possible <- sg_var_names[!(sg_var_names %in% all_grouping_vars)]
     
@@ -227,8 +239,6 @@ summarize_sir_results <- function(sir_df,
     grouping_vars <- all_grouping_vars[!(all_grouping_vars %in% sg_var_names)]
     
     
-    browser()
-    
     #iv) summarize over grouping vars
     sum_pre_tmp <- sir_df_mod %>%
       dplyr::group_by_at(dplyr::vars(grouping_vars, .data$t_icdcat)) %>%
@@ -238,16 +248,14 @@ summarize_sir_results <- function(sir_df,
         group_n_base = sum(.data$n_base, na.rm = TRUE),
         group_incidence_cases = sum(.data$ref_inc_cases, na.rm = TRUE),
         group_population_pyar = sum(.data$ref_population_pyar, na.rm = TRUE),
-        group_min_incidence_crude_rate = min(.data$ref_inc_cases, na.rm = TRUE) / max(.data$ref_population_pyar, na.rm = TRUE) * 100000,
-        group_max_incidence_crude_rate = max(.data$ref_inc_cases, na.rm = TRUE) / min(.data$ref_population_pyar, na.rm = TRUE) * 100000,
-        group_mean_incidence_crude_rate = mean(.data$ref_inc_cases, na.rm = TRUE) / mean(.data$ref_population_pyar, na.rm = TRUE) * 100000,
         group_expected = sum(.data$expected, na.rm = TRUE)) %>%
       dplyr::ungroup() %>%
       #calculate sir
       dplyr::mutate(
         sir = .data$group_observed / .data$group_expected,
         sir_lci = (stats::qchisq(p = alpha / 2, df = 2 * .data$group_observed) / 2) / .data$group_expected,
-        sir_uci = (stats::qchisq(p = 1 - alpha / 2, df = 2 * (.data$group_observed + 1)) / 2) / .data$group_expected
+        sir_uci = (stats::qchisq(p = 1 - alpha / 2, df = 2 * (.data$group_observed + 1)) / 2) / .data$group_expected,
+        group_incidence_crude_rate = .data$group_incidence_cases / .data$group_population_pyar * 100000
       ) %>%
       dplyr::distinct()
     
@@ -256,52 +264,205 @@ summarize_sir_results <- function(sir_df,
     
     
     if("age" %in% sg_var_names == TRUE){
-      sum_pre_tmp <- sum_pre_tmp %>%
-        dplyr::mutate(age = paste0("Total - All included ages: ",  min_age, " - ", max_age))
+      if(shorten_total_cols==FALSE){
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(age = paste0("Total - All included ages: ",  min_age, " - ", max_age))
+      } else{
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(age = "Total")
+      }
     }
     
     if("sex" %in% sg_var_names == TRUE){
-      sum_pre_tmp <- sum_pre_tmp %>%
-        dplyr::mutate(sex = paste0("Total - All included genders: ", paste(used_sex, collapse = ", ")))
+      if(shorten_total_cols==FALSE){
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(sex = paste0("Total - All included genders: ", paste(used_sex, collapse = ", ")))
+      } else{
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(sex = "Total")
+      }
     }
     
     if("region" %in% sg_var_names == TRUE){
-      sum_pre_tmp <- sum_pre_tmp %>%
-        dplyr::mutate(region = paste0("Total - All included regions: ", paste(used_region, collapse = ", ")))
+      if(shorten_total_cols==FALSE){
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(region = paste0("Total - All included regions: ", paste(used_region, collapse = ", ")))
+      } else{
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(region = "Total")
+      }
     }
     
     if("year" %in% sg_var_names == TRUE){
-      sum_pre_tmp <- sum_pre_tmp %>%
-        dplyr::mutate(year = paste0("Total - All included years: ", min_year, " - ", max_year))
+      if(shorten_total_cols==FALSE){
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(year = paste0("Total - All included years: ", min_year, " - ", max_year))
+      } else{
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(year = "Total")
+      }
     }
     
     if("t_icdcat" %in% sg_var_names == TRUE){
-      sum_pre_tmp <- sum_pre_tmp %>%
-        dplyr::mutate(icdcat = paste0("Total - All included ICD categories: ", paste(used_icdcat, collapse = ", ")))
+      if(shorten_total_cols==FALSE){
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(icdcat = paste0("Total - All included ICD categories: ", paste(used_icdcat, collapse = ", ")))
+      } else{
+        sum_pre_tmp <- sum_pre_tmp %>%
+          dplyr::mutate(icdcat = "Total")
+      }
     }
+    
+    #rounding
+    
+    sum_pre_tmp <- sum_pre_tmp %>%
+      dplyr::mutate_at(dplyr::vars(.data$group_pyar, .data$sir, .data$sir_lci, .data$sir_uci), ~ round(.,2))
     
     #do collapse_ci
     if(ci){
-      sum_pre <- sum_pre_tmp %>% 
+      sum_pre_tmp <- sum_pre_tmp %>% 
         tidyr::unite("sir_ci", .data$sir_lci, .data$sir_uci, sep = " - ")
-    } else{
-      sum_pre <- sum_pre_tmp
+    } 
+    
+    #label and sort
+    
+    sum_pre <- sum_pre_tmp %>%
+      dplyr::rename(observed = .data$group_observed,
+                    expected = .data$group_expected,
+                    pyar = .data$group_pyar,
+                    ref_inc_cases = .data$group_incidence_cases,
+                    ref_population_pyar = .data$group_population_pyar,
+                    ref_inc_crude_rate = .data$group_incidence_crude_rate) %>%
+      dplyr::select(dplyr::one_of(c("age", "region", "sex", "year", 
+                                    if(yb){c("yvar_name", "yvar_label")}, if(xb){c("xvar_name", "xvar_label")}, 
+                                    if(fu){"fu_time"}, 
+                                    "t_icdcat", "observed", "expected", "sir",
+                                    if(collapse_ci == TRUE){"sir_ci"},
+                                    if(collapse_ci == FALSE){c("sir_lci", "sir_uci")})),
+                    dplyr::everything()
+      ) %>% 
+      dplyr::arrange_at(dplyr::vars(dplyr::one_of(c("age", "region", "sex", "year", 
+                                                    if(yb){c("yvar_sort", "yvar_sort_levels")}, if(xb){c("xvar_name", "xvar_label")}, 
+                                                    if(fu){"fu_time_sort"}
+      ))))
+    
+    
+  }
+  
+  
+  
+  #change output information required
+  
+  #full
+  if(output_information == "full"){
+    sum_pre2 <- sum_pre
+  }
+  #reduced
+  if(output_information == "reduced"){
+    sum_pre2 <- sum_pre %>%
+      dplyr::select(dplyr::one_of(c("age", "region", "sex", "year", 
+                                    if(yb){c("yvar_name", "yvar_label", "yvar_sort", "yvar_sort_levels")}, 
+                                    if(xb){c("xvar_name", "xvar_label")}, 
+                                    if(fu){c("fu_time", "fu_time_sort")}, 
+                                    "t_icdcat", "observed", "expected", "sir",
+                                    if(collapse_ci == TRUE){"sir_ci"},
+                                    if(collapse_ci == FALSE){c("sir_lci", "sir_uci")},
+                                    "pyar", "n_base")))
+  }
+  #minimal
+  if(output_information == "minimal"){
+    if(!ci){
+      sum_pre <- sum_pre %>%
+        #make sure CIs are minimal
+        tidyr::unite("sir_ci", .data$sir_lci, .data$sir_uci, sep = " - ")
+      
+      ci <- TRUE
     }
     
+    #select only required vars
+    sum_pre2 <- sum_pre %>%
+      dplyr::select(dplyr::one_of(c("age", "region", "sex", "year", 
+                                    if(yb){c("yvar_name", "yvar_label", "yvar_sort", "yvar_sort_levels")}, 
+                                    if(xb){c("xvar_name", "xvar_label")}, 
+                                    if(fu){c("fu_time", "fu_time_sort")}, 
+                                    "t_icdcat", "observed", "expected", "sir", "sir_ci")))
   }
   
   #reshaping according to output option
   
-  #output = "long"
+  #output == "long"
   if(output == "long"){
-    sum_results <- sum_pre
+    sum_results <- sum_pre2
   }
   
   #output = "wide"
+  if(output == "wide"){
+    #wide - only FU-times need to be transposed
+    
+    trans_vars <- names(sum_pre2)[!names(sum_pre2) %in% c("age", "region", "sex", "year", "t_icdcat", 
+                                                          if(yb){c("yvar_name", "yvar_label", "yvar_sort", "yvar_sort_levels")}, 
+                                                          if(xb){c("xvar_name", "xvar_label")}, 
+                                                          if(fu){c("fu_time", "fu_time_sort")})]
+    
+    #creating pivoting specs
+    specs <- sum_pre2 %>% 
+      tidyr::build_wider_spec(names_from = c(if(fu){c("fu_time_sort", "fu_time")}, if(xb){c("xvar_name", "xvar_label")}), 
+                       values_from = trans_vars,
+                       names_sep = ".")
+    
+    if(fu & !xb){
+      specs <- specs %>%
+        dplyr::mutate(.name = paste(paste(fu_time_sort, fu_time, sep = "**"), .value, sep = "__"))
+    }
+    
+    if(!fu & xb){
+      specs <- specs %>%
+        dplyr::mutate(.name = paste(xvar_label, .value, sep = "__"))
+    }
+    
+    if(fu & xb){
+      specs <- specs %>%
+        dplyr::mutate(.name = paste(paste(paste(fu_time_sort, fu_time, sep = "**"), xvar_label, sep = "<<"), .value, sep = "__"))
+    }
+    
+    sum_results_pre <- sum_pre2 %>% 
+      tidyr::pivot_wider_spec(specs)
+    
+    #do sorting
+    
+    #change sort order for fu_time dependent variable names
+    sort <- names(sum_results_pre) %>% stringr::str_subset("__") %>% #detect unique separator
+      stringr::str_replace_all("_exp", "_2exp") %>% stringr::str_replace_all("_obs", "_1obs") %>% #add numer to facilitate sorting
+      stringr::str_replace_all("_sir_uci", "_5sir_uci") %>% stringr::str_replace_all("_sir_lci", "_4sir_lci") %>%
+      stringr::str_replace_all("_sir_ci", "_4sir_ci") %>% stringr::str_replace_all("_sir", "_3sir") %>% 
+      stringr::str_replace_all("_pyar", "_6pyar") %>% stringr::str_replace_all("_group_n_base", "_7group_n_base") %>%
+      stringr::str_sort() %>%
+      stringr::str_replace_all(stringr::regex("\\_[:digit:]"), "_") 
+    
+    
+    #sort dataframe
+    sum_results <- sum_results_pre %>%
+      dplyr::select(dplyr::one_of(c("age", "region", "sex", "year", 
+                                    if(yb){c("yvar_name", "yvar_label")}, 
+                                    "t_icdcat", sort)),
+                    dplyr::everything())
+    
+    names(sum_results) <- names(sum_results) %>% 
+      stringr::str_replace_all(stringr::regex("[:digit:]\\*\\*"), "")
+    
+  }
   
   #output = "nested"
-  
+  if(output == "nested"){
+    
+    sum_results <- sum_pre %>%
+      dplyr::group_by_at(dplyr::vars(grouping_vars)) %>%
+      {if (xb){dplyr::group_by(., .data$xvar_name, add = TRUE)} else{.}} %>% # add x grouping variable if present
+      {if (fu){dplyr::group_by(., .data$fu_time, add = TRUE)} else{.}} %>% # add fub grouping variable
+      tidyr::nest()
+  }
   
   return(sum_results) 
   
 }
+
