9dadd0a4049d8644151569fa3f58376c2d70230a
marianschmidt <marian.schmidt@gmail.com>
2020-03-10 15:31:45 +0100

    remove var_selection option; add datsize option, make sure that timevar_max is enforced on individual level, implement new tidyselect

--- R/reshape_wide_tidyr.R
+++ R/reshape_wide_tidyr.R
@@ -8,52 +8,53 @@
 #'                E.g. \code{timevar="SEQ_NUM"} for SEER data.
 #' @param timevar_max Numeric; default 6. Maximum number of cases per id. 
 #'                    All tumors > timevar_max will be deleted before reshaping.    
-#' @param var_selection Vector of variables to keep. Default is "_all".          
+#' @param datsize Number of rows to be  take from df. This parameter is mainly for testing. Default is Inf so that df is fully processed.
 #' @return df
 #' @export
 #'
 
-reshape_wide_tidyr <- function(df, case_id_var, time_id_var, timevar_max = 6, var_selection = c("_all")){
+reshape_wide_tidyr <- function(df, case_id_var, time_id_var, timevar_max = 6, datsize = Inf){
+  
+  ### restrict size of data.frame to datsize number of rows
+  if(nrow(df) > datsize){
+    df <- df[c(1:datsize), ]
+  }
   
   ### get names from df to provide to pivot function
   trans_vars <- names(df)[!names(df) %in% c({{case_id_var}}, {{time_id_var}})]
   
-  ### limit variables on option var_selection
-  if(all(var_selection != "_all")) {
-    
-    #give warning in case variables defined in var_selection do not exist
-    not_found <- var_selection[!(var_selection %in% trans_vars)]
-    
-    if(length(not_found) > 0) {
-      warning("The following variables defined in var_selection are not found in the formats file: ", not_found)
-    }
-    
-    trans_vars <- trans_vars[trans_vars %in% var_selection]
-    
-  }
   
   ### determine maximum number of cases per patient and deleting all cases > timevar_max
   max_time <- df %>%
-    dplyr::select(time_id_var) %>%
+    dplyr::select(tidyselect::all_of(time_id_var)) %>%
     unlist %>%
     as.numeric %>%
     max(na.rm = TRUE)
+  
   
   if(max_time > timevar_max){
     warning(glue::glue('Long dataset had too many cases per patient. Wide dataset is limited to {timevar_max} cases per id.'))
     
     df <- df %>%
-      dplyr::mutate(counter = as.numeric(.data[[!!time_id_var]])) %>%
+      #sort by case_id and time_id_var
+      dplyr::arrange(!!rlang::sym(case_id_var),!!rlang::sym(time_id_var))%>%
+      #group by case_id_var
+      dplyr::group_by(!!rlang::sym(case_id_var))%>%
+      #calculate new renumbered variable
+      dplyr::mutate(counter = dplyr::row_number()) %>%
+      #filter based on new renumbered variable
       dplyr::filter(counter <= timevar_max) %>%
-      dplyr::select(-counter)
+      dplyr::select(-counter) %>%
+      #ungroup
+      dplyr::ungroup()
     
   }
   
   ### perform tidyr::pivot_wider
   df %>% tidyr::pivot_wider(
     names_from = {{time_id_var}}, 
-    values_from = trans_vars,
+    values_from = tidyselect::all_of(trans_vars),
     names_sep = "."
   )
-
+  
 }
