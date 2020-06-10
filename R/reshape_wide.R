bb14ba86d5ddb6e86a4778e5699b3d409b999848
marianschmidt <marian.schmidt@gmail.com>
2020-06-10 22:37:58 +0200

    update parameter description, add sorting

--- R/reshape_wide.R
+++ R/reshape_wide.R
@@ -2,22 +2,20 @@
 #' Reshape dataset to wide format
 #'
 #' @param df dataframe
-#' @param case_id_var String or vector of strings with name of ID variable indicating same patient.
+#' @param case_id_var String with name of ID variable indicating same patient.
 #'                E.g. \code{idvar="PUBCSNUM"} for SEER data.
 #' @param time_id_var String with name of variable that indicates diagnosis per patient.
 #'                E.g. \code{timevar="SEQ_NUM"} for SEER data.
-#' @param chunks Numeric; default 10.
 #' @param timevar_max Numeric; default 6. Maximum number of cases per id. 
 #'                    All tumors > timevar_max will be deleted before reshaping.              
-#' @param datsize Number of rows to be  take from df. This parameter is mainly for testing. Default is Inf so that df is fully processed.
+#' @param datsize Number of rows to be taken from df. This parameter is mainly for testing. Default is Inf so that df is fully processed.
+#' @param chunks Numeric; default 10. Technical parameter how the data is split during reshaping.
 #' @return df
 #' @export
-#' @importFrom utils str 
-#' @importFrom rlang .data
 #'
 
 
-reshape_wide <- function(df, case_id_var, time_id_var, chunks = 10, timevar_max = 6, datsize = Inf){
+reshape_wide <- function(df, case_id_var, time_id_var, timevar_max = 6, datsize = Inf, chunks = 10){
   
   # restrict size of data.frame to datsize number of rows
   if(nrow(df) > datsize){
@@ -25,24 +23,24 @@ reshape_wide <- function(df, case_id_var, time_id_var, chunks = 10, timevar_max 
   }
   
   #number of patient IDs at start of function
-  n_start <- df %>% dplyr::select(case_id_var) %>% dplyr::n_distinct()
+  n_start <- df %>% dplyr::select(tidyselect::all_of(case_id_var)) %>% dplyr::n_distinct()
   
   #determine maximum number of cases per patient and deleting all cases > timevar_max
   max_time <- df %>%
-    dplyr::select(time_id_var) %>%
+    dplyr::select(tidyselect::all_of(time_id_var)) %>%
     unlist %>%
     as.numeric %>%
     max(na.rm = TRUE)
   
   if(max_time > timevar_max){
-    warning(glue::glue('Long dataset had too many cases per patient. Wide dataset is limited to {timevar_max} cases per id.'))
-
+    rlang::inform(paste("Long dataset had too many cases per patient. Wide dataset is limited to ", timevar_max," cases per id as defined in timevar_max option."))    
+    
     df <- df %>%
-      dplyr::mutate(counter = as.numeric(!! rlang::sym(time_id_var))) %>%
+      dplyr::mutate(counter = as.integer(!! rlang::sym(time_id_var))) %>%
       dplyr::filter(counter <= timevar_max) %>%
       dplyr::select(-counter)
-
-    }
+    
+  }
   
   # split dataset in equal chunks and store in list
   
@@ -63,8 +61,9 @@ reshape_wide <- function(df, case_id_var, time_id_var, chunks = 10, timevar_max 
   }
   
   #rbind chunks into one dataframe
-  wide_df <- dplyr::bind_rows(wide_df) %>% 
-    dplyr::arrange(!! rlang::sym(case_id_var))
+  wide_df <- dplyr::bind_rows(wide_df) %>%
+    #sort by case_id_var
+    dplyr::arrange(as.numeric(rlang::eval_tidy(rlang::ensym(case_id_var))))
   
   #check whether final number of patient IDs matches number at start.
   n_end <- wide_df %>% nrow()
@@ -76,3 +75,4 @@ reshape_wide <- function(df, case_id_var, time_id_var, chunks = 10, timevar_max 
   return(wide_df)
   
 }
+
