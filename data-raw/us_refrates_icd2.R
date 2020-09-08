eddd18a84e7e7facfa941e80385232a1a460a34e
marianschmidt <marian.schmidt@gmail.com>
2020-09-08 16:36:18 +0200

    add sex and race totals to sample data

--- data-raw/us_refrates_icd2.R
+++ data-raw/us_refrates_icd2.R
@@ -37,6 +37,17 @@ age_levels <- c("00 - 04", "05 - 09", "10 - 14", "15 - 19",
 
 race_levels <- c("White", "Black", "Other")
 
+#set n for checks
+
+n_t_site <- length(site_levels)
+n_regyears <- length(region_levels) * length(year_levels)
+n_sex <- length(sex_levels)
+n_age <- length(age_levels)
+n_race <- length(race_levels)
+
+
+#create all combinations of site, region, year, sex, age, race
+
 pre_refrates <- pre_refrates %>%
   complete(t_site = site_levels, region = region_levels, year = year_levels, sex = sex_levels, 
            age = age_levels, race = race_levels)
@@ -45,7 +56,45 @@ pre_refrates <- pre_refrates %>%
   mutate(incidence_cases = sample(c(0:25), size = n(), replace = TRUE),
          population_pyar = sample(c(5000:25000), size = n(), replace = TRUE))
 
-us_refrates_icd2 <- pre_refrates %>%
+#calculate and bind totals
+#race total
+sum_rate_race <- pre_refrates %>%
+  mutate(race_group = "Total - All races") %>%
+  filter(race_group != "") %>%
+  group_by(race_group, t_site, region, year, sex, age) %>%
+  summarise(incidence_cases = sum(incidence_cases),
+            population_pyar = sum(population_pyar)) %>%
+  rename(race = race_group) %>%
+  ungroup()
+
+#check
+if(nrow(sum_rate_race) != (1 * n_age * n_regyears * n_sex * n_t_site)){
+  warning("Check calculations. Number of rows not matching")
+}
+
+pre_refrates2 <- bind_rows(pre_refrates, sum_rate_race) 
+
+#sex groups
+sum_rate_sex <- pre_refrates2 %>%
+  mutate(sex_group = "Total - All genders") %>%
+  filter(sex_group != "") %>%
+  group_by(sex_group, t_site, region, year, age, race) %>%
+  summarise(incidence_cases = sum(incidence_cases),
+            population_pyar = sum(population_pyar)) %>%
+  rename(sex = sex_group) %>%
+  ungroup()
+
+#check
+if(nrow(sum_rate_sex) != ((n_race + 1) * n_age * n_regyears * 1 * n_t_site)){
+  warning("Check calculations. Number of rows not matching")
+}
+
+#merge with existing data
+pre_refrates2 <- bind_rows(pre_refrates2, sum_rate_sex)
+
+
+
+us_refrates_icd2 <- pre_refrates2 %>%
   mutate(population_n_per_year = case_when(stringr::str_length(year) == 4 ~ as.numeric(population_pyar),
                                            stringr::str_length(year) > 4 ~ as.numeric(population_pyar) / 5,
                                            TRUE ~ NA_real_)) %>%
@@ -53,6 +102,8 @@ us_refrates_icd2 <- pre_refrates %>%
   mutate(region = as_factor(region),
          sex = as.factor(sex))
 
-rm(pre_refrates, age_levels, race_levels, region_levels, sex_levels, site_levels, year_levels)
+
+rm(pre_refrates, pre_refrates2, sum_rate_race, sum_rate_sex,
+   age_levels, race_levels, region_levels, sex_levels, site_levels, year_levels)
 
 usethis::use_data(us_refrates_icd2, overwrite = TRUE)
