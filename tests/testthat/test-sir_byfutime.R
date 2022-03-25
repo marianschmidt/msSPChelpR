092000d48c69a3f0fb995e8ff920f3a67ffce16b
marianschmidt <marian.eberl@tum.de>
2022-03-26 00:19:00 +0100

    update test

--- tests/testthat/test-sir_byfutime.R
+++ tests/testthat/test-sir_byfutime.R
@@ -1,7 +1,9 @@
+data("us_second_cancer")
+
 test_that("error thrown for missing variable names if dattype is NULL", {
   expect_error(
     wide_zdata_lung %>%
-      sir_byfutime_dev(
+      sir_byfutime(
         dattype = NULL,
         xbreak_var = "none",
         ybreak_vars = c("t_sublung.1", "t_grading.1"),
@@ -23,22 +25,22 @@ test_that("error thrown for missing variable names if dattype is NULL", {
 
 test_that("correct error is thrown, if dattype = NULL, all var names provided, but one is wrong", {
   expect_error(
-    wide_zdata_lung %>%
-      sir_byfutime_dev(
+    us_second_cancer %>%
+      sir_byfutime(
         dattype = NULL,
         xbreak_var = "none",
-        ybreak_vars = c("t_sublung.1", "t_grading.1"),
+        ybreak_vars = c("registry"),
         futime_breaks = c(0, 1/12, 2/12, 1, 5, 10, Inf),
-        count_var = "count_spc",
+        count_var = "SEQ_NUM",
         refrates_df = rates,
         calc_total_row = TRUE,
         calc_total_fu = TRUE,
-        region_var = "p_region.1",
-        age_var = "t_agediag.1",
-        sex_var = "SEX.1",
+        region_var = "registry",
+        age_var = "fc_age",
+        sex_var = "sex",
         year_var = "wrong_year_var",
-        site_var = "t_icdcat.2",
-        futime_var = "p_futimeyrs",
+        site_var = "t_site_icd",
+        futime_var = "fake_id", 
         alpha = 0.05),
     "The following variables defined are not found in the provided dataframe df: wrong_year_var")
     })
