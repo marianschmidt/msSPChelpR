5446acd7eb394d5eaa57acfe6fac15a4569f0f10
marianschmidt <marian.eberl@tum.de>
2022-04-29 13:54:18 +0200

    remove test

--- tests/testthat/test-sir_byfutime.R
+++ tests/testthat/test-sir_byfutime.R
@@ -23,24 +23,3 @@ test_that("error thrown for missing variable names if dattype is NULL", {
   
 })
 
-test_that("correct error is thrown, if dattype = NULL, all var names provided, but one is wrong", {
-  expect_error(
-    us_second_cancer %>%
-      sir_byfutime(
-        dattype = NULL,
-        xbreak_var = "none",
-        ybreak_vars = c("registry"),
-        futime_breaks = c(0, 1/12, 2/12, 1, 5, 10, Inf),
-        count_var = "SEQ_NUM",
-        refrates_df = rates,
-        calc_total_row = TRUE,
-        calc_total_fu = TRUE,
-        region_var = "registry",
-        age_var = "fc_age",
-        sex_var = "sex",
-        year_var = "wrong_year_var",
-        site_var = "t_site_icd",
-        futime_var = "fake_id", 
-        alpha = 0.05),
-    "The following variables defined are not found in the provided dataframe df: wrong_year_var")
-    })
