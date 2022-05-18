f1242b33c1f573d66f73f7f26bee76f46667ecde
marianschmidt <marian.eberl@tum.de>
2022-05-18 16:40:00 +0200

    implement quiet option

--- R/calc_futime_tt.R
+++ R/calc_futime_tt.R
@@ -5,47 +5,17 @@
 #' @param futime_var_new Name of the newly calculated variable for follow-up time. Default is p_futimeyrs.
 #' @param fu_end end of follow-up in time format YYYY-MM-DD.
 #' @param dattype can be "zfkd" or "seer" or NULL. Will set default variable names if dattype is "seer" or "zfkd". Default is NULL.
-#' @param check Check newly calculated variable p_status by printing frequency table. Default is TRUE.
+#' @param check Check newly calculated variable p_futimeyrs by printing frequency table. Default is TRUE.
 #' @param time_unit Unit of follow-up time (can be "days", "weeks", "months", "years"). Default is "years".
 #' @param status_var Name of the patient status variable that was previously created. Default is p_status.
 #' @param lifedat_var Name of variable containing Date of Death. Will override dattype preset.
 #' @param fcdat_var Name of variable containing Date of Primary Cancer diagnosis. Will override dattype preset.
 #' @param spcdat_var Name of variable containing Date of SPC diagnosis Will override dattype preset.
+#' @param quiet If TRUE, warnings and messages will be suppressed. Default is FALSE.
 #' @return wide_df
 #' @export
-#' @examples 
-#' #load sample data
-#' data("us_second_cancer")
-#' 
-#' #make wide data as this is the required format
-#' usdata_wide <- us_second_cancer %>%
-#'                     msSPChelpR::reshape_wide_tidyr(case_id_var = "fake_id", 
-#'                     time_id_var = "SEQ_NUM", timevar_max = 10)
-#'                     
-#' #prep step - calculate p_spc variable
-#' usdata_wide <- usdata_wide %>%
-#'                  dplyr::mutate(p_spc = dplyr::case_when(is.na(t_site_icd.2)   ~ "No SPC",
-#'                                                        !is.na(t_site_icd.2)   ~ "SPC developed",
-#'                                                        TRUE ~ NA_character_)) %>%
-#'                  dplyr::mutate(count_spc = dplyr::case_when(is.na(t_site_icd.2)   ~ 1,
-#'                                                               TRUE ~ 0))
-#'                                                               
-#' #prep step - create patient status variable
-#' usdata_wide <- usdata_wide %>%
-#'                   msSPChelpR::pat_status(., fu_end = "2017-12-31", dattype = "seer",
-#'                                          status_var = "p_status", life_var = "p_alive.1",
-#'                                          birthdat_var = "datebirth.1", lifedat_var = "datedeath.1")
-#'  
-#' #now we can run the function
-#' msSPChelpR::calc_futime_tt(usdata_wide, 
-#'                         futime_var_new = "p_futimeyrs", 
-#'                         fu_end = "2017-12-31",
-#'                         dattype = "seer", 
-#'                         time_unit = "years",
-#'                         status_var = "p_status",
-#'                         lifedat_var = "datedeath.1", 
-#'                         fcdat_var = "t_datediag.1", 
-#'                         spcdat_var = "t_datediag.2")
+#'
+#'
 #'
 
 calc_futime_tt <- function(wide_df,
@@ -57,52 +27,52 @@ calc_futime_tt <- function(wide_df,
                            status_var = "p_status",
                            lifedat_var = NULL,
                            fcdat_var = NULL,
-                           spcdat_var = NULL){
+                           spcdat_var = NULL,
+                           quiet = FALSE){
   
   #fetch variable names provided in function call
   futime_var_new <- rlang::ensym(futime_var_new)
   status_var <- rlang::ensym(status_var)
   time_unit <- rlang::enquo(time_unit)
   
-  
   if(!is.null(dattype)){
-  #setting default var names and values for SEER data
-  if (dattype == "seer"){
-    if(is.null(lifedat_var)){
-      lifedat_var <- rlang::sym("p_datedeath.1")
-    } else{
-      lifedat_var <- rlang::ensym(lifedat_var)
+    #setting default var names and values for SEER data
+    if (dattype == "seer"){
+      if(is.null(lifedat_var)){
+        lifedat_var <- rlang::sym("p_datedeath.1")
+      } else{
+        lifedat_var <- rlang::ensym(lifedat_var)
+      }
+      if(is.null(fcdat_var)){
+        fcdat_var <- rlang::sym("t_datediag.1")
+      } else{
+        fcdat_var <- rlang::ensym(fcdat_var)
+      }
+      if(is.null(spcdat_var)){
+        spcdat_var <- rlang::sym("t_datediag.2")
+      } else{
+        spcdat_var <- rlang::ensym(spcdat_var)
+      }
     }
-    if(is.null(fcdat_var)){
-      fcdat_var <- rlang::sym("t_datediag.1")
-    } else{
-      fcdat_var <- rlang::ensym(fcdat_var)
+    
+    #setting default var names and values for ZfKD data
+    if (dattype == "zfkd"){
+      if(is.null(lifedat_var)){
+        lifedat_var <- rlang::sym("SDIMP.1")
+      } else{
+        lifedat_var <- rlang::ensym(lifedat_var)
+      }
+      if(is.null(fcdat_var)){
+        fcdat_var <- rlang::sym("DDIMP.1")
+      } else{
+        fcdat_var <- rlang::ensym(fcdat_var)
+      }
+      if(is.null(spcdat_var)){
+        spcdat_var <- rlang::sym("DDIMP.2")
+      } else{
+        spcdat_var <- rlang::ensym(spcdat_var)
+      }
     }
-    if(is.null(spcdat_var)){
-      spcdat_var <- rlang::sym("t_datediag.2")
-    } else{
-      spcdat_var <- rlang::ensym(spcdat_var)
-    }
-  }
-  
-  #setting default var names and values for ZfKD data
-  if (dattype == "zfkd"){
-    if(is.null(lifedat_var)){
-      lifedat_var <- rlang::sym("SDIMP.1")
-    } else{
-      lifedat_var <- rlang::ensym(lifedat_var)
-    }
-    if(is.null(fcdat_var)){
-      fcdat_var <- rlang::sym("DDIMP.1")
-    } else{
-      fcdat_var <- rlang::ensym(fcdat_var)
-    }
-    if(is.null(spcdat_var)){
-      spcdat_var <- rlang::sym("DDIMP.2")
-    } else{
-      spcdat_var <- rlang::ensym(spcdat_var)
-    }
-  }
   } else{
     # ensym if no dattype is given
     lifedat_var <- rlang::ensym(lifedat_var)
@@ -156,16 +126,24 @@ calc_futime_tt <- function(wide_df,
   
   #check whether FU date provided might be too late
   if(fu_end_param > max(wide_df[[rlang::as_name(fcdat_var)]], na.rm = TRUE) & fu_end_param > max(wide_df[[rlang::as_name(spcdat_var)]], na.rm = TRUE)) {
-    rlang::abort(paste0("You have provided an end of Follow-up date that might be out of range of the collected data.",
-                        "Thus events such as SPCs or deaths might not have been recorded and FU-time is overestimated.",
-                        "\nEnd of Follow-up provided: ", fu_end_param,
-                        "\nLatest recorded First Cancer: ",  max(wide_df[[rlang::as_name(fcdat_var)]], na.rm = TRUE),
-                        "\nLatest recorded Second Cancer: ",  max(wide_df[[rlang::as_name(spcdat_var)]], na.rm = TRUE)
-    ))
+    
+    if(!quiet){
+      rlang::warn(c(
+        "i" = "You have provided an end of Follow-up date that might be out of range of the collected data.",
+        "Thus events such as SPCs or deaths might not have been recorded and FU-time is overestimated.",
+        paste0("End of Follow-up provided: ", fu_end_param),
+        paste0("Latest recorded First Cancer: ",  max(wide_df[[rlang::as_name(fcdat_var)]], na.rm = TRUE)),
+        paste0("Latest recorded Second Cancer: ",  max(wide_df[[rlang::as_name(spcdat_var)]], na.rm = TRUE)),
+        "i" = paste0("End of follow-up will be set to: ",  max(wide_df[[rlang::as_name(spcdat_var)]], na.rm = TRUE))
+      ))
+    }
+    
+    fu_end_param <- max(wide_df[[rlang::as_name(spcdat_var)]], na.rm = TRUE)
+    fu_end_quo <- rlang::enquo(fu_end_param)
   }
   
   #check if new and old futime_var are the same --> message that id was overwritten
-  if(rlang::as_name(futime_var_new) %in% names(wide_df)){
+  if(!quiet & rlang::as_name(futime_var_new) %in% names(wide_df)){
     rlang::warn(paste0(rlang::as_name(futime_var_new)," is already present in dataset. Variable has been overwritten with new values."))
   }
   
