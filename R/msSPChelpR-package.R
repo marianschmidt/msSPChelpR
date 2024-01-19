7fde53a6ef98895862d61b63e886dcc02defef96
marianschmidt <marian.schmidt@gmail.com>
2024-01-20 00:40:16 +0100

    replace tidyeval utils

---
+++ R/msSPChelpR-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rlang .data
+#' @importFrom rlang :=
+#' @importFrom rlang as_label
+#' @importFrom rlang as_name
+#' @importFrom rlang enquo
+#' @importFrom rlang enquos
+#' @importFrom rlang eval_tidy
+#' @importFrom rlang expr
+#' @importFrom rlang sym
+#' @importFrom rlang syms
+## usethis namespace: end
+NULL
